040eae40c27fb0a63bd49896d2607e69ab3954cb
navodits <navodit1@gmail.com>
2020-12-04 23:22:43 -0800

    updated backend url

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
--- src/services/photosService.js
+++ src/services/photosService.js
@@ -1,6 +1,6 @@
 import http from "./httpService";
 
-const apiEndpoint = "http://localhost:8000/images/";
+
 
 export function getPhotos() {
     return http.get(apiEndpoint)
