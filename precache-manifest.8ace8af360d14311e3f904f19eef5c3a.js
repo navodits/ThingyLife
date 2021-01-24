309539d5ea4b33278be50bb0ef8f33e36888cd22
navodits <navodit1@gmail.com>
2021-01-24 08:51:45 -0800

    Updates

---
+++ precache-manifest.8ace8af360d14311e3f904f19eef5c3a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40cb4a6a89d346fa3b3805acd10ed7b",
+    "url": "/ThingyLife/index.html"
+  },
+  {
+    "revision": "6fa7b11791e67293eeff",
+    "url": "/ThingyLife/static/css/2.8f191772.chunk.css"
+  },
+  {
+    "revision": "1f1088f58ae01afa850b",
+    "url": "/ThingyLife/static/css/main.bc1a0b93.chunk.css"
+  },
+  {
+    "revision": "6fa7b11791e67293eeff",
+    "url": "/ThingyLife/static/js/2.6f583ff0.chunk.js"
+  },
+  {
+    "revision": "1f1088f58ae01afa850b",
+    "url": "/ThingyLife/static/js/main.ff6d9d70.chunk.js"
+  },
+  {
+    "revision": "a6b0f972c01ec87e58ab",
+    "url": "/ThingyLife/static/js/runtime~main.60e6d973.js"
+  },
+  {
+    "revision": "a0fe4930e171dbba017c40362f15248f",
+    "url": "/ThingyLife/static/media/background.a0fe4930.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ThingyLife/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ThingyLife/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ThingyLife/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ThingyLife/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ThingyLife/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1aa82bfb191be658f03d45aaa216c33b",
+    "url": "/ThingyLife/static/media/pages_background.1aa82bfb.jpg"
+  },
+  {
+    "revision": "d26297ff71fefa72007a89eb7c5496d5",
+    "url": "/ThingyLife/static/media/registration_background.d26297ff.jpg"
+  }
+]);
